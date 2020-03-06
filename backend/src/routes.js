94d73aa374f3ae683ed9b2deeaa5c026dedb3aa3
Lorenalgm <lorena@proesc.com>
2020-03-05 23:39:59 -0300

    Criadas lógicas de cadastro de dev, like e dislike

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,12 @@
-
 const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-	return res.send('HELLO');
-});
-
-routes.post('/devs', (req, res) => {
-	return res.json({ ok: true });
-})
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://teste:teste@omnistack-vgqln.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
+server.use(express.json());
 server.use(routes);
 
 // GET (buscar), POST (criar), PUT (editar), DELETE (deletar)
