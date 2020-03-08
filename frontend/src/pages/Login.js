108f5790525143097b9fc9594e3a7e739d54e24e
Lorenalgm <lorena@proesc.com>
2020-03-08 19:17:29 -0300

    Adicionado main, listando os devs para dar like ou dislike

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+import api from '../services/api';
+
+export default function Login({ history }) {
+	const [username, setUsername] = useState('');
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		const response = await api.post('/devs', {
+			username,
+		});
+
+		const { _id } = response.data;
+
+		history.push(`/dev/${_id}`);
+	}
+
+
   return (
   	<div className="login-container"> 
-  		<form>
+  		<form onSubmit={handleSubmit}>
 	    	<img src={logo} alt="Tindev" />
-	    	<input placeholder="Digite seu usuário no github" />
+	    	<input 
+	    		placeholder="Digite seu usuário no github"
+	    		value={username}
+	    		onChange={e => setUsername(e.target.value)}
+	    	/>
 	    	<button type="submit">Enviar</button>
   		</form>
   	</div>
